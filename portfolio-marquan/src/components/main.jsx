a58d71e87f34cf19e806a85efc02187632a49dcb
MarquanB21 <mbowman@captechconsulting.com>
2023-01-26 11:31:17 -0500

    added work section

--- portfolio-marquan/src/components/main.jsx
+++ portfolio-marquan/src/components/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import { FaTwitter, FaFacebookF, FaLinkedIn, FaInstagram } from "react-icons/fa";
+import { FaTwitter, FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 
-const main = () => {
+const Main = () => {
   return (
     <div id="main">
       <img
@@ -11,9 +11,9 @@ const main = () => {
         alt=''
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div>
-          <h1> Hello, I'm Marquan Bowman</h1>
-          <h2>
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800"> Hello, I'm Marquan Bowman</h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             I'm a
             <TypeAnimation
               sequence={[
@@ -32,11 +32,11 @@ const main = () => {
               style={{ fontSize: '1em', paddingLeft: '5px' }}
             />
           </h2>
-          <div>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
             <FaTwitter className='cursor-pointer' size={20} />
             <FaFacebookF className='cursor-pointer' size={20} />
             <FaInstagram  className='cursor-pointer' size={20}/>
-            <FaLinkedIn className='cursor-pointer' size={20} />
+            <FaLinkedinIn className='cursor-pointer' size={20} />
           </div>
         </div>
       </div>
@@ -44,4 +44,4 @@ const main = () => {
   );
 };
 
-export default main;
+export default Main;
