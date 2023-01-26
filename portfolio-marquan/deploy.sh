726b89ae972f78ee13613181bcf52095626c81b1
MarquanB21 <mbowman@captechconsulting.com>
2023-01-26 18:26:02 -0500

    Update deploy.sh

--- portfolio-marquan/deploy.sh
+++ portfolio-marquan/deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<marquanb21>.github.io/<portfolio>
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<marquanb21>/<portfolio>.git main:gh-pages
 
 cd -
