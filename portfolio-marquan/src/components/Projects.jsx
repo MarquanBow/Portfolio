2c8595cabf5a70a64ca774d465f7a38e55742a61
MarquanB21 <mbowman@captechconsulting.com>
2023-01-27 09:56:17 -0500

    content update

--- portfolio-marquan/src/components/Projects.jsx
+++ portfolio-marquan/src/components/Projects.jsx
@@ -3,12 +3,13 @@ import ProjectItem from './ProjectItem'
 import bouncingball from '../assets/bouncingball.jpg'
 import imagegallery from '../assets/imagegallery.jpg'
 import sillystory from '../assets/sillystory.jpg'
+
 const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
       <p className='text-center py-8'>
-        hdrgjsdvbszl;vsd
+        Here's a running list of all the projects I've done.
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
         <ProjectItem img={bouncingball} title='Bouncing Ball' />
